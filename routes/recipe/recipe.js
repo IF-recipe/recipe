05c23a75ac4400c929ece4b4ece4fc6e312c0c42
parkbeomsoo <proshi@naver.com>
2015-08-19 13:42:58 +0900

    8.19 _ recipe directory 추가

--- routes/recipe/recipe.js
+++ routes/recipe/recipe.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/rest/recipe/addrecipe', function(req, res, next) {
-    console.log("addrecipe");
+router.post('/recipe/add', function(req, res, next) {
+    console.log("add_recipe -----");
+    console.log(req.query);
 });
 
+router.get('/recipe/delete', function(req, res, next) {
+    console.log("delete_recipe -----");
+    console.log(req.query);
 
+});
 
-module.exports = recipe;
+router.get('/recipe/update', function(req, res, next) {
+    console.log("update_recipe -----");
+    console.log(req.query);
+
+});
+
+router.get('/recipe/search', function(req, res, next) {
+    console.log("search_recipe -----");
+    console.log(req.query);
+});
+
+module.exports = router;
--- server_setting/route_use.js
+++ server_setting/route_use.js
@@ -4,11 +4,11 @@
 
 var routes = require('../routes/index');
 var users = require('../routes/users');
+var recipe = require('../routes/recipe/recipe');
 
 exports.routeSetting = function(app){
-
     app.use('/', routes);
-
+    app.use('/rest', recipe);
     app.use('/users', users);
 
 };
