697b91a80483fe34ee72d882989bc797e8401221
parkbeomsoo <proshi@naver.com>
2015-08-19 16:55:08 +0900

    8.19 _ phto Directory 추가

--- routes/photo/photo.js
+++ routes/photo/photo.js
@@ -1,9 +1,19 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
-router.all('/photo/recipe/upload', function(req, res, next) {
+router.post('/photo/recipe/upload', function(req, res, next) {
     console.log("recipe photoUload -----");
-    console.log(req.query);
+    console.log(req.body);
+    console.log(req.head);
+    console.log(req.files);
+    var multiparty = require('multiparty');
+    var form  = new multiparty.Form();
+    form.parse(req, function(err, fields, files){
+
+    });
+
+
 });
 
 router.all('/photo/profile/upload', function(req, res, next) {
