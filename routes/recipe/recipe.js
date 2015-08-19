7c51f96b106f42197a31de38e76530f30b1df1d6
parkbeomsoo <proshi@naver.com>
2015-08-19 13:27:16 +0900

    8.19 _ recipe directory 추가

---
+++ routes/recipe/recipe.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/rest/recipe/addrecipe', function(req, res, next) {
+    console.log("addrecipe");
+});
+
+
+
+module.exports = recipe;
