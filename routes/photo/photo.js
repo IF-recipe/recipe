06db883cf6db5ef78b237acfcc64883a57ef8ae6
parkbeomsoo <proshi@naver.com>
2015-08-20 17:40:34 +0900

    8.19 _ phto Directory 추가

--- routes/photo/photo.js
+++ routes/photo/photo.js
@@ -1,32 +1,44 @@
 var fs = require('fs');
-var multiparty = require('multiparty');
+var multer  = require('multer')
+var upload = multer()
 var express = require('express');
 var router = express.Router();
 
-router.post('/photo/recipe/upload', function(req, res, next) {
+router.post('/photo/recipe/upload',upload.array(), function (req, res, next) {
     console.log("recipe photoUload -----req.url : " + req.url);
     console.log(req.body);
     console.log(req.headers);
     console.log(req.params);
     console.log(req.query);
     console.log(req.files);
-    console.log("------------req end")
-    var form = new multiparty.Form();
+    console.log("------------req end");
+    var storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, '/tmp/my-uploads')
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + '-' + Date.now())
+        }
+    })
+
+})
 
 
-    form.parse(req, function(err, fields, files) {
-        Object.keys(fields).forEach(function(name){
-            console.log('got field named ' + name + "// value : " + fields.value);
-        });
-
-        Object.keys(files).forEach(function(name) {
-            console.log('got file named ' + name);
-        });
-
-        console.log(fields);
-        console.log(files);
-    });
-});
+    //var form = new multiparty.Form();
+    //
+    //
+    //form.parse(req, function(err, fields, files) {
+    //    Object.keys(fields).forEach(function(name){
+    //        console.log('got field named ' + name + "// value : " + fields.value);
+    //    });
+    //
+    //    Object.keys(files).forEach(function(name) {
+    //        console.log('got file named ' + name);
+    //    });
+    //
+    //    console.log(fields);
+    //    console.log(files);
+    //});
 
 router.all('/photo/profile/upload', function(req, res, next) {
     console.log("profile photoUload -----");
