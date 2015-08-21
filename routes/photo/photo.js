d49b6d151d459f19c0cfd8e6caf2b7b1d8c2e79b
parkbeomsoo <proshi@naver.com>
2015-08-21 14:00:48 +0900

    8.20 _ phto Directory edit [add download]

--- routes/photo/photo.js
+++ routes/photo/photo.js
@@ -11,7 +11,7 @@ router.get('/photo/recipe/download', function(req, res, next){
 });
 
 router.post('/photo/recipe/upload',upload.single('files'), function (req, res, next) {
-    console.log("recipe photoUload -----req.url : " + req.url);
+    console.log("recipe photo Upload -----req.url : " + req.url);
     console.log(req.body);
     console.log(req.headers);
     console.log(req.params);
