90d0314c82d03f1a23f0f229dbe59ee0481340f3
parkbeomsoo <proshi@naver.com>
2015-08-21 15:09:03 +0900

    8.20 add download

--- routes/photo/photo.js
+++ routes/photo/photo.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-//var multer  = require('multer');
-//var upload = multer({dest : "uploads/"});
+var multer  = require('multer');
+var upload = multer({dest : "uploads/"});
 
 router.get('/photo/recipe/download', function(req, res, next){
     console.log("reciep photo DownLoad ------");
@@ -10,20 +10,20 @@ router.get('/photo/recipe/download', function(req, res, next){
     var stream = fs.createReadStream(__dirname + "/../../uploads/1");
     stream.pipe(res);
 });
-//router.post('/photo/recipe/upload',upload.single('files'), function (req, res, next) {
-//    console.log("recipe photo Upload -----req.url : " + req.url);
-//    console.log(req.body);
-//    console.log(req.headers);
-//    console.log(req.params);
-//    console.log(req.query);
-//    console.log(req.file);
-//    console.log(req.files);
-//    console.log("------------req end");
-//    /**
-//     *  -add Mongo DB insert Query call
-//     */
-//
-//})
+router.post('/photo/recipe/upload',upload.single('files'), function (req, res, next) {
+    console.log("recipe photo Upload -----req.url : " + req.url);
+    console.log(req.body);
+    console.log(req.headers);
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.file);
+    console.log(req.files);
+    console.log("------------req end");
+    /**
+     *  -add Mongo DB insert Query call
+     */
+
+})
 
 
 
