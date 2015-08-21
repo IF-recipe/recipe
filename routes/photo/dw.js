c7bbcd39755f0e508d53293b8e839935f3c53419
parkbeomsoo <proshi@naver.com>
2015-08-21 15:32:29 +0900

    8.20 add download

--- routes/photo/dw.js
+++ routes/photo/dw.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-router.get('/photo/recipe/dw', function(req, res, next){
+router.all('/photo/recipe/dw', function(req, res, next){
     console.log("reciep photo DownLoad ------");
 
     var stream = fs.createReadStream(__dirname + "/../../uploads/1");
