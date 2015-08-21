8ef4cfb629e277a229c8512d67bbc20c2f9fad60
parkbeomsoo <proshi@naver.com>
2015-08-21 15:17:29 +0900

    8.20 add download

--- routes/photo/photo.js
+++ routes/photo/photo.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+
 var multer  = require('multer');
 var upload = multer({dest : "uploads/"});
 
-router.get('/photo/recipe/download', function(req, res, next){
-    console.log("reciep photo DownLoad ------");
 
-    var stream = fs.createReadStream(__dirname + "/../../uploads/1");
-    stream.pipe(res);
-});
-router.post('/photo/recipe/upload',upload.single('files'), function (req, res, next) {
+
+router.post('/photo/recipe/upload', upload.single('files'), function (req, res, next) {
     console.log("recipe photo Upload -----req.url : " + req.url);
     console.log(req.body);
     console.log(req.headers);
