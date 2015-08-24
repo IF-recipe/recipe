647fda6d6b56502e57a98e877b67835d262af5bf
parkbeomsoo <proshi@naver.com>
2015-08-24 18:13:10 +0900

    8.24 modify photo.js -- add [:photoId] restfule

--- routes/photo/photo.js
+++ routes/photo/photo.js
@@ -4,9 +4,9 @@ var multer  = require('multer');
 var upload = multer({dest : "uploads/"});
 var fs = require('fs');
 
-router.get('/photo/recipe/download', function(req, res, next){
-    console.log("reciep photo DownLoad ------");
-    var stream = fs.createReadStream(__dirname + "/../../uploads/f42b5ea95b164e4a7690925c46263c65");
+router.get('/photo/recipe/download/:photoId', function(req, res, next){
+    console.log("reciep photo DownLoad ------>", req.params.photoId);
+    var stream = fs.createReadStream(__dirname + "/../../uploads/"+req.params.photoId);
     stream.pipe(res);
 });
 
